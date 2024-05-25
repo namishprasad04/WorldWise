4f8db96e849a4a46a5580c27191f90b995f77b8c
Namish Prasad <namishprasad180@gmail.com>
2024-05-25 19:02:16 +0530

    project done

---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+import styles from './Button.module.css'
+
+export default function Button({children,onClick,type}) {
+  return (
+    <button onClick={onClick} className={`${styles.btn} ${styles[type]}`}>{children}</button>
+  )
+}
